ba7a2e2fe6639a26d8608797370f6cf1e4da457a
alroysh <cicak.jatuh02@gmail.com>
2017-04-07 13:30:27 +0800

    Update line_class.php

--- line_class.php
+++ line_class.php
@@ -1,25 +1,56 @@
-<?php 
-if (!function_exists('hash_equals')) 
-{
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * This polyfill of hash_equals() is a modified edition of https://github.com/indigophp/hash-compat/tree/43a19f42093a0cd2d11874dff9d891027fc42214
+ *
+ * Copyright (c) 2015 Indigo Development Team
+ * Released under the MIT license
+ * https://github.com/indigophp/hash-compat/blob/43a19f42093a0cd2d11874dff9d891027fc42214/LICENSE
+ */
+if (!function_exists('hash_equals')) {
     defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
+
     function hash_equals($knownString, $userString)
     {
         $strlen = function ($string) {
             if (USE_MB_STRING) {
                 return mb_strlen($string, '8bit');
             }
+
             return strlen($string);
         };
+
+        // Compare string lengths
         if (($length = $strlen($knownString)) !== $strlen($userString)) {
             return false;
         }
+
         $diff = 0;
+
+        // Calculate differences
         for ($i = 0; $i < $length; $i++) {
             $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
         }
         return $diff === 0;
     }
 }
+
 class LINEBotTiny
 {
     public function __construct($channelAccessToken, $channelSecret)
@@ -27,8 +58,7 @@ class LINEBotTiny
         $this->channelAccessToken = $channelAccessToken;
         $this->channelSecret = $channelSecret;
     }
-	
-	
+
     public function parseEvents()
     {
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -36,17 +66,21 @@ class LINEBotTiny
             error_log("Method not allowed");
             exit();
         }
+
         $entityBody = file_get_contents('php://input');
+
         if (strlen($entityBody) === 0) {
             http_response_code(400);
             error_log("Missing request body");
             exit();
         }
+
         if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
             http_response_code(400);
             error_log("Invalid signature value");
             exit();
         }
+
         $data = json_decode($entityBody, true);
         if (!isset($data['events'])) {
             http_response_code(400);
@@ -55,12 +89,14 @@ class LINEBotTiny
         }
         return $data['events'];
     }
+
     public function replyMessage($message)
     {
         $header = array(
             "Content-Type: application/json",
             'Authorization: Bearer ' . $this->channelAccessToken,
         );
+
         $context = stream_context_create(array(
             "http" => array(
                 "method" => "POST",
@@ -68,27 +104,14 @@ class LINEBotTiny
                 "content" => json_encode($message),
             ),
         ));
-		$response = exec_url('https://api.line.me/v2/bot/message/reply',$this->channelAccessToken,json_encode($message));
-    }
-	
-    public function pushMessage($message) 
-    {
-        
-		$response = exec_url('https://api.line.me/v2/bot/message/push',$this->channelAccessToken,json_encode($message));
-       
-    }
-	
-	   public function leaveGroup($groupId)
-    {
-        return $this->httpClient->delete($this->endpointBase . 'https://api.line.me/v2/bot/group/{groupId}/leave' . urlencode($groupId) . '/leave', []);
+
+        $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
+        if (strpos($http_response_header[0], '200') === false) {
+            http_response_code(500);
+            error_log("Request failed: " . $response);
+        }
     }
 
-    public function profil($userId)
-    {
-      
-		return json_decode(exec_get('https://api.line.me/v2/bot/profile/'.$userId,$this->channelAccessToken));
-       
-    }
     private function sign($body)
     {
         $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
@@ -96,64 +119,3 @@ class LINEBotTiny
         return $signature;
     }
 }
-function exec_get($fullurl,$channelAccessToken)
-{
-		
-		$header = array(
-            "Content-Type: application/json",
-            'Authorization: Bearer '.$channelAccessToken,
-        );
-		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_VERBOSE, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);		
-		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-		curl_setopt($ch, CURLOPT_URL, $fullurl);
-		
-		$returned =  curl_exec($ch);
-	
-		return($returned);
-}
-function exec_url($fullurl,$channelAccessToken,$message)
-{
-		
-		$header = array(
-            "Content-Type: application/json",
-            'Authorization: Bearer '.$channelAccessToken,
-        );
-		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_VERBOSE, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_POST,           1 );
-		curl_setopt($ch, CURLOPT_POSTFIELDS,     $message); 
-		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-		curl_setopt($ch, CURLOPT_URL, $fullurl);
-		
-		$returned =  curl_exec($ch);
-	
-		return($returned);
-}
-function exec_url_aja($fullurl)
-	{
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_VERBOSE, 1);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-			curl_setopt($ch, CURLOPT_URL, $fullurl);
-			
-			$returned =  curl_exec($ch);
-		
-			return($returned);
-	}
